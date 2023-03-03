6efe0e4ca566c7e010309da4a92f95c1b0af2a66
skmudassir-it <96885904+skmudassir-it@users.noreply.github.com>
2023-03-03 03:51:05 +0000

    one

---
+++ food-app/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { Navbar } from './components/navbar';
+function App() {
+  return 
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/'>
+          </Route>
+          <Route path='/cart'>
+          </Route>
+        </Routes>
+      </Router>
+    </div>;
+}
+
+export default App;
